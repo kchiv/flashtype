69423f93c124045c0411ea50b93fbfb78a71e590
Kyle Chivers <chiverskyle@gmail.com>
2023-01-08 14:18:19 -0800

    move text to App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class App extends React.Component {
+    render () {
+        return (
+            <h1>Hey There! My Name Is Kyle!</h1>
+        )
+    }
+}
+
+export default App;
