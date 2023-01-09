e5b3a2a2526f9b6d8402fbbfe7f6034b528169a6
Kyle Chivers <chiverskyle@gmail.com>
2023-01-08 16:59:28 -0800

    outlined app components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,15 @@ import React from "react";
 class App extends React.Component {
     render () {
         return (
-            <h1>Hey There! My Name Is Kyle!</h1>
+            <div className="app">
+                {/* Nav Section */}
+
+                {/* Landing Page */}
+
+                {/* Challenge Section */}
+
+                {/* Footer */}
+            </div>
         )
     }
 }
