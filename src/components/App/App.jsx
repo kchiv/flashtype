6e8bb55f23dc2bec219d0d19196179d5d49aa5ef
Kyle Chivers <chiverskyle@gmail.com>
2023-01-29 11:16:58 -0800

    testing css

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import "./App.css";
 
 class App extends React.Component {
     render () {
         return (
             <div className="app">
                 {/* Nav Section */}
-
+                <h1>This is the nav bar!</h1>
                 {/* Landing Page */}
 
                 {/* Challenge Section */}
